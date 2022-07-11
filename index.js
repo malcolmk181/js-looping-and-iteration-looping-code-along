a184844e7cb3b39311d0c5c0f9ddf99443f5ed5a
Malcolm Keyes <3.malcolmk@gmail.com>
2022-07-11 11:10:38 -0400

    commit tree b9a38e01c151ebb60898fd71d4d04d897f458173

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName) {
+    let returnArray = [];
+    for (let i = 0; i < names.length; i++) {
+        returnArray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return returnArray;
+}
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+}
